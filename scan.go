b53c33482ca281ae1790c57e7515cb4fd76102a5
David McIntosh <804610+mctofu@users.noreply.github.com>
2022-09-27 22:33:54 -0700

    sort map constants

--- scan.go
+++ scan.go
@@ -13,18 +13,18 @@ import (
 )
 
 var tagExts = map[string]struct{}{
-	".mp3":  {},
-	".m4a":  {},
 	".flac": {},
+	".m4a":  {},
+	".mp3":  {},
 	".ogg":  {},
-	".wav":  {},
 	".opus": {},
+	".wav":  {},
 }
 
 var imgExts = map[string]struct{}{
+	".gif": {},
 	".jpg": {},
 	".png": {},
-	".gif": {},
 }
 
 type WalkFunc func(dir *PathMeta, file *PathMeta) error
